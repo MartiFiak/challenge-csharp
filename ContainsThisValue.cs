3d06a067a4be091e014401fd237779dd58b0449c
MartiFiak <jacolotmarine@gmail.com>
2023-10-09 16:14:40 +0200

    ContainsThisValue

--- ContainsThisValue.cs
+++ ContainsThisValue.cs
@@ -4,6 +4,9 @@ namespace CSharpDiscovery.Quest02
     {
         public static bool ContainsThisValue(int[] tab, int value)
         {
+            if (value == 0) {
+                return false;
+            }
             return tab.Contains(value);
         }
     }
