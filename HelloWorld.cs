0422f2b6d0c03a0d3f35c0dd30a3bae95fe19d97
MartiFiak <jacolotmarine@gmail.com>
2023-10-09 10:05:23 +0200

    HelloWorld

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -4,7 +4,7 @@ namespace CSharpDiscovery.Quest01
     {
         public static string HelloWorld()
         {
-            return ('Hello World !');
+            return ("Hello World !");
         }
     }
 }
