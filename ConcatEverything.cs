da2010771c2a2b74a82b5a95c08c8304962c121d
MartiFiak <jacolotmarine@gmail.com>
2023-10-09 14:17:39 +0200

    ConcatEverything

---
+++ ConcatEverything.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics.Contracts;
+
+namespace CSharpDiscovery.Quest01
+{
+    public class ConcatEverything_Exercice
+    {
+        public static string ConcatEverything(params string[] str)
+        {
+            return string.Join("", str.ToArray());
+        }
+    }
+}
