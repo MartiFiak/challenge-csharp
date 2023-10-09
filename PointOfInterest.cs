dd82464661bf71d7c93af5f98464a2008bc84b4b
MartiFiak <jacolotmarine@gmail.com>
2023-10-09 22:03:22 +0200

    PointOfInterest

---
+++ PointOfInterest.cs
@@ -0,0 +1,22 @@
+using System.Dynamic;
+
+namespace CSharpDiscovery.Quest03;
+
+public class PointOfInterest {
+    public Double Latitude {
+        get; set;
+    }
+    public Double Longitude {
+        get; set;
+    }
+    public String? Name {
+        get; set;
+    }
+    public static String GoogleMapsUrlTemplate {
+        get; set; 
+    }
+
+    static PointOfInterest() {
+        GoogleMapsUrlTemplate = "https://www.google.com/maps/search/?api=1&query={0},{1}";
+    }
+}
